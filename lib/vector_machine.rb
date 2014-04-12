de8ebf84b0ce88b9ba8f145e7a7e71ef3fe4921e
Andrew Smith <andrewchristophersmith@gmail.com>
2014-04-12 11:21:24 -0700

    Added support for n-dimensions to #get_adjacent_pairs

--- lib/vector_machine.rb
+++ lib/vector_machine.rb
@@ -7,11 +7,13 @@ class VectorMachine
     out_shape[0] -= 1
     
     out = NMatrix.zeros(out_shape, dtype: vector.dtype)
+    args = out.shape.map {:*}
     # out_new_shape = out.rank(0, 0...out.shape[0]-1).shape
     # first = NMatrix.new(out_new_shape, vector.rank(0, 0...vector.shape[0]-1).to_a.flatten, dtype: vector.dtype)
     # second = NMatrix.new(out_new_shape, vector.rank(0, 1...vector.shape[0]).to_a.flatten, dtype: vector.dtype)
-    out.rank(out.dim-1, 0, :reference)[:*, :*, :*] = vector.rank(0, 0...vector.shape[0]-1)
-    out.rank(out.dim-1, 1, :reference)[:*, :*, :*] = vector.rank(0, 1...vector.shape[0])
-    
+    out_shape[0] -= 1
+    out.rank(0, 0, :reference)[*args]= vector.rank(0, 0...vector.shape[0]-1).reshape(out_shape)
+    out.rank(0, 1, :reference)[*args]= vector.rank(0, 1...vector.shape[0]).reshape(out_shape)
+    out
   end
 end
--- test/test_vector_machine.rb
+++ test/test_vector_machine.rb
@@ -9,12 +9,22 @@ class TestVectorMachine < MiniTest::Unit::TestCase
   
   # Testing on 1D vectors
   def test_gets_adjacent_pairs_1d
-    assert_equal @vm.get_adjacent_pairs(@m), N[[0, 1], [1, 2]]
+    assert_equal N[[0, 1], [1, 2]], @vm.get_adjacent_pairs(@m)
   end
   
   # Testing on 2D vectors
   def test_gets_adjacent_pairs_2d
-    @m = ::NMatrix.new([3, 2], [0, 1, 2, 3, 4, 5])
-    assert_equal @vm.get_adjacent_pairs(@m), N[[[0, 1], [2, 3]], [[2, 3], [4, 5]]]
+    @m = ::NMatrix.seq([3, 2])
+    assert_equal N[[[0, 1], [2, 3]], [[2, 3], [4, 5]]], @vm.get_adjacent_pairs(@m)
+  end
+  
+  # Testing on 3D vectors
+  def test_gets_adjacent_pairs_3d
+    @m = ::NMatrix.seq([3,3,2])
+    exp = [
+      [[[0, 1], [2, 3], [4, 5]], [[6, 7], [8, 9], [10, 11]]],
+      [[[6, 7], [8, 9], [10, 11]], [[12, 13], [14, 15], [16, 17]]]
+    ]
+    assert_equal NMatrix.new([2,2,3,2], exp.flatten), @vm.get_adjacent_pairs(@m)
   end
 end
