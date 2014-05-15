f6f4477957c243d75f3703d599d9bdd0e553c426
Andrew Smith <andrewchristophersmith@gmail.com>
2014-05-14 19:37:52 -0700

    Whoops.

---
+++ lib/mm/deltas.rb
@@ -0,0 +1,8 @@
+module MM
+  class Deltas
+    def self.abs n
+      (n[0] - n[1]).abs
+    end
+  end
+end
+
---
+++ lib/mm/scaling.rb
@@ -0,0 +1,16 @@
+module MM
+  module Scaling
+    def self.none pairs
+      pairs
+    end
+    def self.absolute pairs
+      max = (pairs.map(&:max)).max
+      pairs.map {|x| x.map {|y| y.to_f / max}}
+    end
+    def self.relative pairs
+      maxes = pairs.map(&:max)
+      pairs.zip(maxes).map {|pair, max| pair.map {|x| x.to_f / max}}
+    end
+  end
+end
+
