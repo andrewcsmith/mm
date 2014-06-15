412d483f8e811a5d3a7350a2f2bbe3029c554bcb
Andrew Smith <andrewchristophersmith@gmail.com>
2014-06-15 14:42:22 -0700

    Refacted pairs methods to avoid aliasing.

--- lib/mm/pairs.rb
+++ lib/mm/pairs.rb
@@ -1,49 +1,11 @@
 module MM
   class Pairs
-    # Checks to see whether an Object responds to various methods. Raises
-    # ArgumentError if #respond_to? returns false.
-    #
-    # obj       - Object to check for responses
-    # methods   - Array of Symbols to check for response
-    #
-    # Returns true if all responses succeeded.
-    def responds_to_arguments(obj, methods)
-      methods.each do |x|
-        if !obj.respond_to? x
-          raise ArgumentError, "#{obj.class} does not implement #{x}"
-        end
-      end
-      true
-    end
-
-    # General method to get adjacent pairs
-    def get_adjacent_pairs(vector)
+    def linear vector
       vector.each_cons(2).to_a
     end
-
-    # Combinatorial pairs of an NMatrix's outermost dimension
-    #
-    # Benchmarking results [2014-04-28]:
-    # 
-    #     bench_gets_combinatorial_pairs_1d  0.000070  0.000149  0.001146  0.010921  0.147744
-    #     bench_gets_combinatorial_pairs_2d  0.000115  0.000374  0.003322  0.037855  0.413232
-    #
-    def get_combinatorial_pairs(vector)
-      # getting the combinations via the normal array method
-      combos = vector.to_a.combination(2).to_a
-      if vector.class == combos.class
-        return combos
-      end
-      # get the proper shape
-      # this should be obsolete once we have Array#to_nm fixed
-      # out_shape = get_pairs_shape(vector, :combinatorial)
-      # set up the output matrix
-      # NMatrix.new(out_shape, combos.flatten, dtype: vector.dtype, stype: vector.stype)
-      raise Exception, "oops."
+    def combinatorial vector
+      vector.combination(2).to_a
     end
-
-    alias_method :linear, :get_adjacent_pairs
-    alias_method :combinatorial, :get_combinatorial_pairs
   end
 end
 
--- test/mm/test_pairs.rb
+++ test/mm/test_pairs.rb
@@ -8,30 +8,20 @@ class TestMM::TestPairs < Minitest::Test
     @pairs = MM::Pairs.new
   end
 
-  def test_responds_to_arguments_validates_true
-    assert @pairs.responds_to_arguments([], [:each, :[]])
+  def test_linear_flat_array
+    assert_equal [[0, 1], [1, 2], [2, 3]], @pairs.linear([0, 1, 2, 3])
   end
 
-  def test_responds_to_arguments_raises_exception
-    assert_raises(ArgumentError) do
-      @pairs.responds_to_arguments([], [:lol])
-    end
+  def test_linear_nested_array
+    assert_equal [[[0, 1], [2, 3]], [[2, 3], [4, 5]]], @pairs.linear([[0, 1], [2, 3], [4, 5]])
   end
 
-  def test_get_adjacent_pairs_flat_array
-    assert_equal [[0, 1], [1, 2], [2, 3]], @pairs.get_adjacent_pairs([0, 1, 2, 3])
-  end
-
-  def test_get_adjacent_pairs_nested_array
-    assert_equal [[[0, 1], [2, 3]], [[2, 3], [4, 5]]], @pairs.get_adjacent_pairs([[0, 1], [2, 3], [4, 5]])
-  end
-
-  def test_get_combinatorial_pairs_flat_array
-    assert_equal [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]], @pairs.get_combinatorial_pairs([0, 1, 2, 3])
+  def test_combinatorial_flat_array
+    assert_equal [[0, 1], [0, 2], [0, 3], [1, 2], [1, 3], [2, 3]], @pairs.combinatorial([0, 1, 2, 3])
   end
   
-  def test_get_combinatorial_pairs_nested_array
-    assert_equal [[[0, 1], [2, 3]], [[0, 1], [4, 5]], [[2, 3], [4, 5]]], @pairs.get_combinatorial_pairs([[0, 1], [2, 3], [4, 5]])
+  def test_combinatorial_nested_array
+    assert_equal [[[0, 1], [2, 3]], [[0, 1], [4, 5]], [[2, 3], [4, 5]]], @pairs.combinatorial([[0, 1], [2, 3], [4, 5]])
   end
 end
 
