2aea9f315261762e404eca274abec325659bd6e5
Andrew Smith <andrewchristophersmith@gmail.com>
2014-06-26 21:14:34 -0700

    Added global max meta-scaling method

--- lib/mm/scaling.rb
+++ lib/mm/scaling.rb
@@ -16,6 +16,12 @@ module MM
       maxes = pairs.map(&:max)
       pairs.zip(maxes).map {|pair, max| pair.map {|x| x.to_f / max}}
     end
+    # Note: a bit hacky. But anything starting with "get_" should be considered
+    # a meta-scaling method. This method returns a Proc that has a particular
+    # scaling value hard-coded into it, for re-use and re-use.
+    def self.get_global max
+      ->(pairs) {pairs.map {|x| x.map {|y| y.to_f / max}}}
+    end
   end
 end
 
--- test/mm/test_scaling.rb
+++ test/mm/test_scaling.rb
@@ -11,19 +11,26 @@ class TestMM::TestScaling < Minitest::Test
     expected = [[0.667, 0.5, 1.0, 0.333],
                 [0.5, 0.667, 0.167, 0.333]]
     result = MM::Scaling.absolute([@n, @m])
-    result.zip(expected) do |x|
-      x[0].zip(x[1]) do |y| 
-        assert_in_delta *y, 0.001
-      end
-    end
+    assert_nested_values expected, result
   end
   def test_relative_scaling
     expected = [[0.667, 0.5, 1.0, 0.333],
                 [0.75, 1.0, 0.25, 0.5]]
     result = MM::Scaling.relative([@n, @m])
-    result.zip(expected) do |x|
-      x[0].zip(x[1]) do |y| 
-        assert_in_delta *y, 0.001
+    assert_nested_values expected, result
+  end
+  def test_get_global_scaling
+    expected = [[0.5, 0.375, 0.75, 0.25],
+                [0.375, 0.5, 0.125, 0.25]]
+    result = MM::Scaling.get_global(8.0).call([@n, @m])
+    assert_nested_values expected, result
+  end
+
+  # Asserts that nested values 2-deep are within a certain delta
+  def assert_nested_values exp, act, delta = 0.001, msg = nil
+    exp.zip(act) do |x|
+      x[0].zip(x[1]) do |y|
+        assert_in_delta *y, delta, msg
       end
     end
   end
