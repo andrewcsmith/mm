7bafb6a716bc10f28ea03d75b4606f3bc1a16fd4
Andrew Smith <andrewchristophersmith@gmail.com>
2014-07-04 10:19:46 -0600

    Added ratio.rb to Manifest

--- lib/mm.rb
+++ lib/mm.rb
@@ -2,9 +2,9 @@ module MM
   VERSION = "1.0.0"
 end
 
-require 'mm/pairs.rb'
-require 'mm/metric.rb'
-require 'mm/deltas.rb'
-require 'mm/scaling.rb'
-require 'mm/ratio.rb'
+require 'mm/pairs'
+require 'mm/metric'
+require 'mm/deltas'
+require 'mm/scaling'
+require 'mm/ratio'
 
