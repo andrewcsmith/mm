87f66c9dcd96fed13b815fb3deae75cb8c1462bf
Andrew Smith <andrewchristophersmith@gmail.com>
2014-07-05 20:48:11 -0700

    Added extra coverage to MM::Deltas

--- test/mm/test_deltas.rb
+++ test/mm/test_deltas.rb
@@ -1,8 +1,11 @@
 require 'mm/deltas'
+require_relative '../helpers.rb'
 
 class TestMM < Minitest::Test; end
 
 class TestMM::TestDeltas < Minitest::Test
+  include TestHelpers
+
   def test_abs_ordered_inter_delta
     input = [[5, 12], [4, 2], [3, 11], [6, 7]]
     exp = [7, 2, 8, 1]
@@ -24,18 +27,17 @@ class TestMM::TestDeltas < Minitest::Test
   def test_tenney_ordered_intra_delta
     input = [[Rational(1,1), Rational(3,2)], [Rational(3,2), Rational(5,4)]]
     exp = [2.585, 4.907]
-    result = input.map {|x| MM::Deltas.tenney(x)}
-    result.zip(exp).each do |n|
-      assert_in_delta(*n, 0.001)
-    end
+    assert_nested_in_delta exp, input.map {|x| MM::Deltas.tenney(x)}
   end
   def test_log_ratio_ordered_intra_delta
     input = [[Rational(1,1), Rational(3,2)], [Rational(3,2), Rational(5,4)]]
     exp = [0.585, 0.263]
-    result = input.map {|x| MM::Deltas.log_ratio(x)}
-    result.zip(exp).each do |n|
-      assert_in_delta(*n, 0.001)
-    end
+    assert_nested_in_delta exp, input.map {|x| MM::Deltas.log_ratio(x)}
+  end
+  def test_ratio_ordered_intra_delta
+    input = [[Rational(1,1), Rational(3,2)], [Rational(3,2), Rational(5,4)]]
+    exp = [Rational(2,3), Rational(6,5)]
+    assert_nested_in_delta exp, input.map {|x| MM::Deltas.ratio(x)}
   end
 end
 
