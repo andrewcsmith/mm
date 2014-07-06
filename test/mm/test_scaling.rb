7c482e9647eaaa265d7717e42fa1277341405cc6
Andrew Smith <andrewchristophersmith@gmail.com>
2014-07-05 20:37:09 -0700

    Refactored test helpers into a module

--- test/mm/test_scaling.rb
+++ test/mm/test_scaling.rb
@@ -1,8 +1,11 @@
 require 'mm/scaling'
+require_relative '../helpers.rb'
 
 class TestMM < Minitest::Test; end
 
 class TestMM::TestScaling < Minitest::Test
+  include TestHelpers
+
   def setup
     @n = [4, 3, 6, 2]
     @m = [3, 4, 1, 2]
@@ -11,28 +14,19 @@ class TestMM::TestScaling < Minitest::Test
     expected = [[0.667, 0.5, 1.0, 0.333],
                 [0.5, 0.667, 0.167, 0.333]]
     result = MM::Scaling.absolute([@n, @m])
-    assert_nested_values expected, result
+    assert_nested_in_delta_2_deep expected, result
   end
   def test_relative_scaling
     expected = [[0.667, 0.5, 1.0, 0.333],
                 [0.75, 1.0, 0.25, 0.5]]
     result = MM::Scaling.relative([@n, @m])
-    assert_nested_values expected, result
+    assert_nested_in_delta_2_deep expected, result
   end
   def test_get_global_scaling
     expected = [[0.5, 0.375, 0.75, 0.25],
                 [0.375, 0.5, 0.125, 0.25]]
     result = MM::Scaling.get_global(8.0).call([@n, @m])
-    assert_nested_values expected, result
-  end
-
-  # Asserts that nested values 2-deep are within a certain delta
-  def assert_nested_values exp, act, delta = 0.001, msg = nil
-    exp.zip(act) do |x|
-      x[0].zip(x[1]) do |y|
-        assert_in_delta(*y, delta, msg)
-      end
-    end
+    assert_nested_in_delta_2_deep expected, result
   end
 end
 
