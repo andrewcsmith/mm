8ba6daac8230c613879be4f8eb32c5463e2086a8
Andrew Smith <andrewchristophersmith@gmail.com>
2014-07-15 20:24:11 -0700

    Added returns after ends

--- lib/mm/deltas.rb
+++ lib/mm/deltas.rb
@@ -3,21 +3,26 @@ module MM
     def self.abs n
       (n[0] - n[1]).abs
     end
+
     def self.mean n
       n.inject(0.0, :+) / n.size
     end
+
     # Have to scale by 0.5 in order to normalize to a max distance of 1.0
     def self.direction n
       (n[0] <=> n[1]) * 0.5
     end
+
     # Accepts a tuple where the quotient responds to #numerator and #denominator
     def self.tenney n
       ->(r) { Math.log2(r.numerator * r.denominator) }.call(n[0] / n[1])
     end
+
     # Accepts a tuple of anything that Math.log2 can handle
     def self.log_ratio n
       Math.log2((n[0] / n[1]).to_f).abs
     end
+
     def self.ratio n
       n[0] / n[1]
     end
