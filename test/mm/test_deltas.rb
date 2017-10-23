40e1747ea992874df236c0b7b82ce27752982cb1
Andrew C. Smith <andrewchristophersmith@gmail.com>
2017-10-22 22:17:35 -0700

    Add hard returns, update gems

--- test/mm/test_deltas.rb
+++ test/mm/test_deltas.rb
@@ -12,28 +12,33 @@ class TestMM::TestDeltas < Minitest::Test
     result = input.map {|x| MM::Deltas.abs(x)}
     assert_equal exp, result
   end
+
   def test_mean_inter_delta
     input = [[5, 4, 3, 6], [12, 2, 11, 7]]
     exp = [4.5, 8]
     result = input.map {|x| MM::Deltas.mean(x)}
     assert_equal exp, result
   end
+
   def test_distance_ordered_inter_delta
     input = [[-1, -1], [1, 1], [-1, 1], [-1, -1]]
     exp = [0, 0, 2, 0]
     result = input.map {|x| MM::Deltas.abs(x)}
     assert_equal exp, result
   end
+
   def test_tenney_ordered_intra_delta
     input = [[Rational(1,1), Rational(3,2)], [Rational(3,2), Rational(5,4)]]
     exp = [2.585, 4.907]
     assert_nested_in_delta exp, input.map {|x| MM::Deltas.tenney(x)}
   end
+
   def test_log_ratio_ordered_intra_delta
     input = [[Rational(1,1), Rational(3,2)], [Rational(3,2), Rational(5,4)]]
     exp = [0.585, 0.263]
     assert_nested_in_delta exp, input.map {|x| MM::Deltas.log_ratio(x)}
   end
+
   def test_ratio_ordered_intra_delta
     input = [[Rational(1,1), Rational(3,2)], [Rational(3,2), Rational(5,4)]]
     exp = [Rational(2,3), Rational(6,5)]
